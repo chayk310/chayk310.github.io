c538b3cb57677301c1dd99e4dc9ed97084794d70
chayk310 <46476765+chayk310@users.noreply.github.com>
2024-02-01 15:06:04 +0900

    Update js_1.js

--- assets/js_1.js
+++ assets/js_1.js
@@ -1,3 +1,6 @@
-function abc(){
-  
-}
+ function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://web1-adxdsxsysl.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    }();
