f474a1c141c18a9642b078929ad5e5d4e4137f9b
chayk310 <46476765+chayk310@users.noreply.github.com>
2024-02-03 11:53:43 +0900

    Update js_1.js

--- assets/js_1.js
+++ assets/js_1.js
@@ -1,1 +1,27 @@
+function nightDayHandler(self){
+  
+		var target= document.querySelector('body');
+		var alist = document.querySelectorAll('a');
+		if(self.value === 'night'){
+			target.style.backgroundColor='black';
+			target.style.color='white';
+			self.value = 'day';
 
+			var i=0;
+			while(i<alist.length){
+				alist[i].style.color='powderblue';
+				i++;
+			}
+		}else{
+			target.style.backgroundColor='white';
+			target.style.color='black';
+			self.value ='night';
+
+			var i=0;
+			while(i<alist.length){
+				alist[i].style.color='blue';
+				i++;
+			}
+	}
+
+}
